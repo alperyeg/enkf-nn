321a5e50f30807161d38ab64b2b140d0c674cdd0
yegenoglu <yegenoglu@frau-holle.igpm.rwth-aachen.de>
2020-07-08 12:00:29 +0200

    benchmark stuff

---
+++ benchmark/enkf_pytorch_bak.py
@@ -0,0 +1,281 @@
+import numpy as np
+import abc
+import time
+import torch
+from abc import ABCMeta
+from torch.nn.functional import one_hot
+
+# TODO: revise the documentation
+
+
+class KalmanFilter(metaclass=ABCMeta):
+
+    @abc.abstractmethod
+    def fit(self, data, ensemble, ensemble_size, moments1,
+            observations, model_output, gamma, noise):
+        pass
+
+
+class EnsembleKalmanFilter(KalmanFilter):
+    def __init__(self, tol, maxit, stopping_crit, n_batches=32,
+                 shuffle=True, online=False, converge=False,
+                 loss_function='norm'):
+        """
+        Ensemble Kalman Filter (EnKF)
+
+        EnKF following the formulation found in Iglesias et al. (2013), 
+        The Ensemble Kalman Filter for Inverse Problems.
+
+        :param tol: float, tolerance value for convergence
+        :param maxit: int, maximum number of iterations
+        :param stopping_crit: str, stopping criterion,
+            `discrepancy`: checks if the actual misfit is smaller or equal
+            to the noise
+            `relative`: checks if the absolute value between actual and
+            previous misfit is smaller than given tolerance `tol`
+            otherwise calculates the loss between iteration `n` and `n-1` and
+            stops if the difference is `< tol`
+        :param shuffle: bool, True if the dataset should be shuffled,
+            Default is `True`.
+        :param n_batches, int,  number of batches to used in mini-batch. If set
+            to `1` uses the whole given dataset. Default is `32`.
+        :param online, bool, True if one random data point is requested,
+            between [0, dims], otherwise do mini-batch, Default is False
+        :param converge, bool, Checks and stops the iteration and updating step
+            if convergence is reached, Default is `False`.
+        :param loss_function, string, name of the loss function
+           `MAE` is the Mean Absolute Error or l1 - loss
+           `MSE` is the Mean Squared Error or l2 - loss
+           `CE` cross-entropy loss
+           `norm` norm-2 or Frobenius norm,
+           Default is `norm`
+
+        """
+        self.device = torch.device("cuda" if torch.cuda.is_available()
+                                   else "cpu")
+        self.Cpp = None
+        self.Cup = None
+        self.ensemble = None
+        self.observations = None
+        self.data = None
+        self.e = None
+        self.r = None
+        self.misfit = None
+        self.u_exact = None
+        self.norm_uexact2 = None
+        self.norm_p2 = None
+        self.m1 = None
+
+        self.maxit = maxit
+        self.shuffle = shuffle
+        self.stopping_crit = stopping_crit
+        self.tol = tol
+        self.converge = converge
+        self.online = online
+        self.n_batches = n_batches
+        self.loss_function = loss_function
+        self.gamma = 0.
+        self.gamma_s = 0
+        self.dims = 0
+        self.times = {}
+
+    def fit(self, data, ensemble, ensemble_size, moments1, observations,
+            model_output, gamma, noise=0.):
+        """
+        Prediction and update step of the EnKF
+
+        Calculates new ensembles.
+
+        :param ensemble: nd numpy array, contains ensembles `u`
+        :param ensemble_size: int, number of ensembles
+        :param observations: nd numpy array, observation or targets
+        :param model_output: nd numpy array, output of the model
+            In terms of the Kalman Filter the model maps the ensembles (dim n)
+            into the observed data `y` (dim k). E.g. network output activity
+        :param noise: nd numpy array, Noise can be added to the model (for `gamma`) 
+            and is used in the misfit calculation for convergence.
+            E.g. multivariate normal distribution. Default is `0.0`
+        :param  gamma: nd numpy array, Normalizes the model-data distance in the
+            update step, :`noise * I` (I is identity matrix) or
+            :math:`\\gamma=I` if `noise` is zero
+        :return self, Possible outputs are, if calculated:
+            ensembles: nd numpy array, optimized `ensembles`
+            Cpp: nd numpy array, covariance matrix of the model output
+            Cup: nd numpy array, covariance matrix of the model output and the ensembles 
+        """
+        # get shapes
+        self.gamma_s, self.dims = _get_shapes(observations, model_output)
+
+        if isinstance(gamma, (int, float)):
+            if float(gamma) == 0.:
+                self.gamma = np.eye(self.gamma_s)
+        else:
+            self.gamma = gamma
+
+        self.times = {'Cpp':[], 'Cup':[], 'update':[], 'lstsq': [], 'mm': []} 
+
+        # copy the data so we do not overwrite the original arguments
+        self.ensemble = ensemble.clone()
+        self.observations = observations.clone()
+        self.observations = _encode_targets(observations, self.gamma_s)
+        self.data = data.clone()
+        # convert to pytorch
+        self.ensemble = torch.as_tensor(
+            self.ensemble, device=self.device, dtype=torch.float32)
+        self.observations = torch.as_tensor(
+            self.observations, device=self.device, dtype=torch.float32)
+        self.data = torch.as_tensor(self.data, device=self.device)
+        self.gamma = torch.as_tensor(
+            self.gamma, device=self.device, dtype=torch.float32)
+        model_output = torch.as_tensor(
+            model_output, device=self.device, dtype=torch.float32)
+
+        for i in range(self.maxit):
+            if (i % 100) == 0:
+                print('Iteration {}/{}'.format(i, self.maxit))
+            if self.shuffle:
+                data, observations = _shuffle(self.data, self.observations)
+
+            # now get mini_batches
+            if self.n_batches > self.dims:
+                num_batches = 1
+            else:
+                num_batches = self.n_batches
+            mini_batches = _get_batches(
+                num_batches, shape=self.dims, online=self.online)
+            mini_batches = torch.as_tensor(mini_batches, device=self.device)
+            for idx in mini_batches:
+                # in case of online learning idx should be an int
+                # put it into a list to loop over it
+                for d in idx:
+                    # now get only the individuals output according to idx
+                    g_tmp = model_output[:, :, d]
+                    # Calculate the covariances
+                    torch.cuda.synchronize(self.device)
+                    t = time.time()
+                    Cpp = _cov_mat(g_tmp, g_tmp, ensemble_size)
+                    self.times['Cpp'].append(time.time()-t)
+                    t = time.time()
+                    Cup = _cov_mat(self.ensemble, g_tmp, ensemble_size)
+                    self.times['Cup'].append(time.time()-t)
+                    t = time.time()
+                    self.ensemble = _update_step(self.ensemble,
+                                                 self.observations[d],
+                                                 g_tmp, self.gamma, Cpp, Cup, self.times,
+                                                 self.device)
+                    self.times['update'].append(time.time()-t)
+
+            # m = torch.distributions.Normal(self.ensemble.mean(),
+            #                                self.ensemble.std())
+            # self.ensemble += m.sample(self.ensemble.shape)
+            # cov = 0.01 * _cov_mat(self.ensemble, self.ensemble, ensemble_size)
+            # rnd = torch.randn(
+            #     size=(self.ensemble.shape[1], ensemble_size), device=self.device)
+            # mm = torch.mm(cov, rnd).to(self.device)
+            # self.ensemble += mm.t()
+        print('Cpp', Cpp)
+        return self
+
+
+def _update_step(ensemble, observations, g, gamma, Cpp, Cup, times, device):
+    """
+    Update step of the kalman filter
+    Calculates the covariances and returns new ensembles
+    """
+    # return ensemble + (Cup @ np.linalg.lstsq(Cpp+gamma, (observations - g).T)[0]).T
+    return torch.mm(Cup, torch.lstsq((observations-g).t(), Cpp+gamma)[0]).t() + ensemble
+    # c = Cpp + gamma
+    # o = observations-g
+    # o = o.t()
+    # t = time.time()
+    # lstsq = torch.lstsq(o, c)[0]
+    # times['lstsq'].append(time.time() - t)
+    # t = time.time()
+    # r = torch.mm(Cup, lstsq)
+    # r = r.t()
+    # times['mm'].append(time.time() - t)
+    # return r + ensemble
+
+
+def _cov_mat(x, y, ensemble_size):
+    """
+    Covariance matrix
+    """
+    # x_bar = _get_mean(x)
+    # y_bar = _get_mean(y)
+
+    # cov = 0.0
+    return torch.tensordot((x - x.mean(0)), (y - y.mean(0)),
+                           dims=([0], [0])) / ensemble_size
+
+
+def _get_mean(x):
+    """
+    Depending on the shape returns the correct mean
+    """
+    if x.shape[1] == 1:
+        return np.mean(x)
+    return np.mean(x, axis=0)
+
+
+def _get_shapes(observations, model_output):
+    """
+    Returns individual shapes
+
+    :returns gamma_shape, length of the observation (here: size of last layer
+                          of network)
+    :returns dimensions, number of observations (and data)
+    """
+    if len(model_output.size()) > 2:
+        gamma_shape = model_output.shape[1]
+    else:
+        gamma_shape = model_output.shape[0]
+    dimensions = observations.shape[0]
+    return gamma_shape, dimensions
+
+
+def _one_hot_vector(index, shape):
+    """
+    Encode targets into one-hot representation
+    """
+    target = np.zeros(shape)
+    target[index] = 1.0
+    return target
+
+
+def _encode_targets(targets, shape):
+    #return np.array(
+    #    [_one_hot_vector(targets[i], shape) for i in range(targets.shape[0])])
+    return one_hot(targets, shape).float()
+    
+
+
+def _shuffle(data, targets):
+    """
+    Shuffles the data and targets by permuting them
+    """
+    indices = np.random.permutation(targets.shape[0])
+    return data[indices], targets[indices]
+
+
+def _get_batches(n_batches, shape, online):
+    """
+    :param n_batches, int, number of batches
+    :param shape, int, shape of the data
+    :param online, bool, True if one random data point is requested,
+                         between [0, dims], otherwise do mini-batch
+    """
+    if online:
+        return [np.random.randint(0, shape)]
+    else:
+        num_batches = n_batches
+        mini_batches = _mini_batches(shape=shape,
+                                     n_batches=num_batches)
+        return mini_batches
+
+
+def _mini_batches(shape, n_batches):
+    """
+    Splits the data set into `n_batches` of shape `shape`
+    """
+    return np.array_split(range(shape), n_batches)
---
+++ benchmark/enkf_pytorch_conv-run.py
@@ -0,0 +1,419 @@
+import copy
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import torch.nn as nn
+import time
+# import matplotlib.pyplot as plt
+from numpy.linalg import norm
+import numpy as np
+from conv_net import ConvNet
+from enkf_pytorch import EnsembleKalmanFilter as EnKF
+from enkf_pytorch import _encode_targets
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class DataLoader:
+    """
+    Convenience class.
+
+
+    """
+
+    def __init__(self):
+        self.data_mnist = None
+        self.test_mnist = None
+        self.data_mnist_loader = None
+        self.test_mnist_loader = None
+
+    def init_iterators(self, root, batch_size):
+        self.data_mnist_loader, self.test_mnist_loader = self.load_data(
+            root, batch_size)
+        # here are the expensive operations
+        self.data_mnist = self._make_iterator(self.data_mnist_loader)
+        self.test_mnist = self._make_iterator(self.test_mnist_loader)
+
+    @staticmethod
+    def _make_iterator(iterable):
+        """
+        Return an iterator for a given iterable.
+        Here `iterable` is a pytorch `DataLoader`
+        """
+        return iter(iterable)
+
+    @staticmethod
+    def load_data(root, batch_size):
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize([0.], [1.])
+             ])
+        # load now MNIST dataset
+        trainset_mnist = torchvision.datasets.MNIST(root=root,
+                                                    train=True,
+                                                    download=True,
+                                                    transform=transform)
+        trainloader_mnist = torch.utils.data.DataLoader(trainset_mnist,
+                                                        batch_size=batch_size,
+                                                        shuffle=False,
+                                                        pin_memory=True,
+                                                        num_workers=2)
+
+        testset_mnist = torchvision.datasets.MNIST(root=root,
+                                                   train=False,
+                                                   download=True,
+                                                   transform=transform)
+        testload_mnist = torch.utils.data.DataLoader(testset_mnist,
+                                                     batch_size=batch_size,
+                                                     shuffle=False,
+                                                     pin_memory=True,
+                                                     num_workers=2)
+        return trainloader_mnist, testload_mnist
+
+    def dataiter_mnist(self):
+        """ MNIST training set list iterator """
+        return self.data_mnist.next()
+
+    def testiter_mnist(self):
+        """ MNIST test set list iterator """
+        return self.test_mnist.next()
+
+
+class MnistOptimizee(torch.nn.Module):
+    def __init__(self, seed, n_ensembles, batch_size, root):
+        super(MnistOptimizee, self).__init__()
+
+        self.random_state = np.random.RandomState(seed=seed)
+
+        self.n_ensembles = n_ensembles
+        self.batch_size = batch_size
+        self.root = root
+
+        self.conv_net = ConvNet().to(device)
+        self.criterion = nn.CrossEntropyLoss()
+        self.data_loader = DataLoader()
+        self.data_loader.init_iterators(self.root, self.batch_size)
+        self.dataiter_mnist = self.data_loader.dataiter_mnist
+        self.testiter_mnist = self.data_loader.testiter_mnist
+
+        self.generation = 0
+        self.inputs, self.labels = self.dataiter_mnist()
+        self.inputs = self.inputs.to(device)
+        self.labels = self.labels.to(device)
+        self.test_input, self.test_label = self.testiter_mnist()
+        self.test_input = self.test_input.to(device)
+        self.test_label = self.test_label.to(device)
+
+        self.timings = {'shape_parameters': [], 'set_parameters': [],
+                        'set_parameters_cnn': [], 'shape_parameters_ens': [], 'fit':[]}
+        
+        self.length = 0
+        for key in self.conv_net.state_dict().keys():
+            self.length += self.conv_net.state_dict()[key].nelement()
+
+
+    def create_individual(self):
+        # get weights, biases from networks and flatten them
+        # convolutional network parameters ###
+        conv_ensembles = []
+        with torch.no_grad():
+            # weights for layers, conv1, conv2, fc1
+            # conv1_weights = self.conv_net.state_dict()['conv1.weight'].view(-1).numpy()
+            # conv2_weights = self.conv_net.state_dict()['conv2.weight'].view(-1).numpy()
+            # fc1_weights = self.conv_net.state_dict()['fc1.weight'].view(-1).numpy()
+
+            # bias
+            # conv1_bias = self.conv_net.state_dict()['conv1.bias'].numpy()
+            # conv2_bias = self.conv_net.state_dict()['conv2.bias'].numpy()
+            # fc1_bias = self.conv_net.state_dict()['fc1.bias'].numpy()
+
+            # stack everything into a vector of
+            # conv1_weights, conv1_bias, conv2_weights, conv2_bias,
+            # fc1_weights, fc1_bias
+            # params = np.hstack((conv1_weights, conv1_bias, conv2_weights,
+            #                     conv2_bias, fc1_weights, fc1_bias))
+            # length = 0
+            # for key in self.conv_net.state_dict().keys():
+            #     length += self.conv_net.state_dict()[key].nelement()
+
+            # conv_ensembles.append(params)
+            # l = np.random.uniform(-.5, .5, size=length)
+            for _ in range(self.n_ensembles):
+                #     stacked = []
+                #     for key in self.conv_net.state_dict().keys():
+                #         stacked.extend(
+                #             self._he_init(self.conv_net.state_dict()[key]))
+                #     conv_ensembles.append(stacked)
+                # tmp = []
+                # for j in l:
+                #     jitter = np.random.uniform(-0.1, 0.1) + j
+                #     tmp.append(jitter)
+                # conv_ensembles.append(tmp)
+                conv_ensembles.append(np.random.normal(0, 0.1,
+                                                       size=self.length))
+            return dict(conv_params=torch.as_tensor(conv_ensembles,
+                                                    device=device),
+                        targets=self.labels,
+                        input=self.inputs.squeeze())
+
+    def load_model(self, path='conv_params.npy'):
+        print('Loading model from path: {}'.format(path))
+        conv_params = np.load(path).item()
+        conv_ensembles = conv_params.get('ensemble')
+        return dict(conv_params=torch.as_tensor(conv_ensembles,
+                                                device=device),
+                    targets=self.labels,
+                    input=self.inputs.squeeze())
+
+    @staticmethod
+    def _he_init(weights, gain=0):
+        """
+        He- or Kaiming- initialization as in He et al., "Delving deep into
+        rectifiers: Surpassing human-level performance on ImageNet
+        classification". Values are sampled from
+        :math:`\\mathcal{N}(0, \\text{std})` where
+
+        .. math::
+        \text{std} = \\sqrt{\\frac{2}{(1 + a^2) \\times \text{fan\\_in}}}
+
+        Note: Only for the case that the non-linearity of the network
+            activation is `relu`
+
+        :param weights, tensor
+        :param gain, additional scaling factor, Default is 0
+        :return: numpy nd array, random array of size `weights`
+        """
+        fan_in = torch.nn.init._calculate_correct_fan(weights, 'fan_in')
+        stddev = np.sqrt(2. / fan_in * (1 + gain ** 2))
+        return np.random.normal(0, stddev, weights.numel())
+
+    def set_parameters(self, ensembles):
+        # set the new parameter for the network
+        conv_params = ensembles.mean(0)
+        t = time.time()
+        ds = self._shape_parameter_to_conv_net(conv_params)
+        self.timings['shape_parameters_ens'].append(time.time()-t)
+        t = time.time()
+        self.conv_net.set_parameter(ds)
+        self.timings['set_parameters_cnn'].append(time.time()-t)
+        print('---- Train -----')
+        print('Generation ', self.generation)
+        generation_change = 1
+        with torch.no_grad():
+            inputs = self.inputs.to(device)
+            labels = self.labels.to(device)
+
+            if self.generation % generation_change == 0:
+                self.inputs, self.labels = self.dataiter_mnist()
+                self.inputs = self.inputs.to(device)
+                self.labels = self.labels.to(device)
+                print('New MNIST set used at generation {}'.format(
+                    self.generation))
+
+            outputs, act1, act2 = self.conv_net(inputs)
+            conv_loss = self.criterion(outputs, labels).item()
+            train_cost = _calculate_cost(_encode_targets(labels, 10),
+                                         F.softmax(outputs, dim=1), 'MSE')
+            train_acc = score(labels,
+                              torch.argmax(F.softmax(outputs, dim=1), 1))
+            print('Cost: ', train_cost)
+            print('Accuracy: ', train_acc)
+            print('Loss:', conv_loss)
+
+            print('---- Test -----')
+            test_output, act1, act2 = self.conv_net(self.test_input)
+            test_loss = self.criterion(test_output, self.test_label).item()
+            test_acc = score(self.test_label,
+                             torch.argmax(test_output, 1))
+            test_cost = _calculate_cost(_encode_targets(self.test_label, 10),
+                                        test_output, 'MSE')
+            print('Test accuracy', test_acc)
+            print('Test loss: {}'.format(test_loss))
+            print('-----------------')
+            conv_params = []
+            for idx, c in enumerate(ensembles):
+                t = time.time()
+                ds = self._shape_parameter_to_conv_net(c)
+                self.timings['shape_parameters'].append(time.time()-t)
+                t = time.time()            
+                self.conv_net.set_parameter(ds)
+                self.timings['set_parameters'].append(time.time()-t)
+                params, _, _ = self.conv_net(inputs)
+                conv_params.append(params.t())
+            conv_params = torch.stack(conv_params)
+            outs = {
+                'conv_params': conv_params,
+                'conv_loss': float(conv_loss),
+                'input': self.inputs.squeeze(),
+                'targets': self.labels
+            }
+        return outs
+        
+    def _shape_parameter_to_conv_net(self, params):
+        param_dict = dict()
+        start = 0
+        for key in self.conv_net.state_dict().keys():
+            shape = self.conv_net.state_dict()[key].shape
+            length = self.conv_net.state_dict()[key].nelement()
+            end = start + length
+            param_dict[key] = params[start:end].reshape(shape)
+            start = end
+        return param_dict
+
+
+def _calculate_cost(y, y_hat, loss_function='CE'):
+    """
+    Loss functions
+    :param y: target
+    :param y_hat: calculated output (here G(u) or feed-forword output a)
+    :param loss_function: name of the loss function
+           `MAE` is the Mean Absolute Error or l1 - loss
+           `MSE` is the Mean Squared Error or l2 - loss
+           `CE` cross-entropy loss, requires `y_hat` to be in [0, 1]
+           `norm` norm-2 or Forbenius norm of `y - y_hat`
+    :return: cost calculated according to `loss_function`
+    """
+    if loss_function == 'CE':
+        term1 = -y * torch.log(y_hat)
+        term2 = (1 - y) * torch.log(1 - y_hat)
+        return torch.sum(term1 - term2)
+    elif loss_function == 'MAE':
+        return torch.sum(np.absolute(y_hat - y)) / len(y)
+    elif loss_function == 'MSE':
+        return torch.sum((y_hat - y) ** 2) / len(y)
+    elif loss_function == 'norm':
+        return norm(y - y_hat)
+    else:
+        raise KeyError(
+            'Loss Function \'{}\' not understood.'.format(loss_function))
+
+
+def score(x, y):
+    """
+    :param x: targets
+    :param y: prediction
+    :return:
+    """
+    print('target ', x)
+    print('predict ', y)
+    n_correct = (y == x).sum().item()
+    n_total = float(len(y))
+    sc = n_correct / n_total
+    return sc
+
+
+def plot_distributions(dist, title='Distribution'):
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    ax.set_xlabel('Digits')
+    ax.set_ylabel('Frequency')
+    ax.hist(dist)
+    plt.show()
+
+
+def jitter_ensembles(ens, ens_size):
+    res = []
+    for _ in range(ens_size):
+        res.append(ens + np.random.uniform(-0.01, 0.01, len(ens)))
+    return np.array(res)
+
+
+def test(net, iteration, test_loader_mnist, criterion):
+    net.eval()
+    test_accuracy = 0
+    test_loss = 0
+    with torch.no_grad():
+        for idx, (img, target) in enumerate(test_loader_mnist):
+            img = img.to(device)
+            target = target.to(device)
+            output, _, _ = net(img)
+            loss = criterion(output, target)
+            test_loss += loss.item()
+            # network prediction
+            pred = output.argmax(1, keepdim=True)
+            # how many image are correct classified, compare with targets
+            test_accuracy += pred.eq(target.view_as(pred)).sum().item()
+
+            if idx % 10 == 0:
+                print('Test Loss {} in iteration {}, idx {}'.format(
+                    loss.item(), iteration, idx))
+        ta = 100 * test_accuracy / len(test_loader_mnist.dataset)
+        tl = test_loss / len(test_loader_mnist.dataset)
+        print('------ Evaluation -----')
+        print('Test accuracy: {} Average test loss: {}'.format(ta, tl))
+        print('-----------------------')
+    return ta, tl
+
+
+def dict_values_to_numpy(dct):
+    d = {}
+    for k, v in dct.items():
+        if isinstance(v, torch.Tensor):
+            d[k] = v.cpu().numpy()
+        else:
+            d[k] = v
+    return d
+
+
+if __name__ == '__main__':
+    timings = {}
+    root = '../multitask'
+    n_ensembles = 5000
+    conv_loss_mnist = []
+    # average test losses
+    test_losses = []
+    np.random.seed(0)
+    torch.manual_seed(0)
+    batch_size = 64
+    model = MnistOptimizee(root=root, batch_size=batch_size, seed=0,
+                           n_ensembles=n_ensembles).to(device)
+    conv_ens = None
+    gamma = np.eye(10) * 0.01
+    enkf = EnKF(tol=1e-5,
+                maxit=1,
+                stopping_crit='',
+                online=False,
+                shuffle=False,
+                n_batches=1,
+                converge=False)
+    rng = int(60000 / batch_size * 8)
+    for i in range(1):
+        model.generation = i + 1
+        if i == 0:
+            try:
+                out = model.load_model('')
+                # replace cov matrix with cov from weights (ensembles)
+                # m = torch.distributions.Normal(out['conv_params'].mean(),
+                #                                out['conv_params'].std())
+                # model.cov = m.sample((n_ensembles, model.length))
+            except FileNotFoundError as fe:
+                print(fe)
+                print('Model not found! Initalizaing new ensembles.')
+                out = model.create_individual()
+            conv_ens = out['conv_params']  # + torch.as_tensor(model.cov)
+            t = time.time()
+            out = model.set_parameters(conv_ens)
+            model.timings['set_parameters_cnn'].append(time.time()-t)
+
+        t = time.time()
+        enkf.fit(data=out['input'],
+                 ensemble=conv_ens,
+                 ensemble_size=n_ensembles,
+                 moments1=conv_ens.mean(0),
+                 observations=out['targets'],
+                 model_output=out['conv_params'], noise=0.0,
+                 gamma=gamma)
+        model.timings['fit'].append(time.time() - t)
+        timings[str(i)] = {**model.timings, **enkf.times}
+        print('done in {} s'.format(time.time() - t))
+        conv_ens = enkf.ensemble
+        t = time.time()
+        model.set_parameters(conv_ens)
+        print('model set params time ', time.time() - t)
+        model.timings['set_parameters_cnn'].append(time.time()-t)
+        
+    last_key = np.sort([int(j) for j in list(timings)])[-1] + 1
+    timings['{}'.format(last_key)] = {**model.timings, **enkf.times}
+    torch.save(timings, 'timings_ens{}_ep{}.pt'.format(n_ensembles, last_key))
